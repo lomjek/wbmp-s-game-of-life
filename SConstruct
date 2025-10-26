871fd6e3dd5dd455f26f29cb42e9b6313b1947ad
lomjek <leondergrunder@gmail.com>
2025-10-26 16:00:50 +0100

    CMD Input properly implemented

--- SConstruct
+++ SConstruct
@@ -1,7 +1,28 @@
 env = Environment()
 
-if ARGUMENTS.get('debug'):
-    env.Append(CCFLAGS=['-g'])
+target = ARGUMENTS.get('target', 'debug').lower()
 
-env.Append(CPPPATH=['#']) 
+if target == 'debug':
+    print("Building DEBUG configuration...")
+    env.Append(
+        CCFLAGS = ['-g', '-O0'],
+        CPPDEFINES = ['DEBUG']
+    )
+
+elif target == 'release':
+    print("Building RELEASE configuration...")
+    env.Append(
+        CCFLAGS = ['-Oz', '-DNDEBUG', '-Wextra', '-Wall'],
+        CPPDEFINES = ['RELEASE']
+    )
+
+else:
+    print(f"Error: Unknown target '{target}'. Use 'debug' or 'release'.")
+    Exit(1)
+
+
+env.Append(
+    CPPPATH=['#'],
+    LIBS=['m']
+) 
 env.Program('game_of_life', Glob('src/*.c'))
--- src/main.c
+++ src/main.c
@@ -2,11 +2,18 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <limits.h>
 
 #include "headers/wbmp_io.h"
 
 unsigned int height = 0;
 unsigned int width = 0;
+
+unsigned long current_step = 0;
+unsigned long final_step = 0;
+size_t  used_digits = 0;
 
 size_t raw_data_size;
 
@@ -21,19 +28,44 @@ int main(int argc, char *argv[]){
         printf("You did not provide enough ingormation.\n\tTo use this app you must use:\n\texecutable <path_to_original_image> <number_of_iterations>\n\nThis App quit!\n");
         return 1;
     }
-    printf("Loading first frame!\n"); 
+   
     char *original_frame = argv[1];
+    char *frame_count = argv[2];
+
+    //Handle the animation length input
+    char *empty;
+    long raw_fc = strtol(frame_count, &empty, 0);
+    
+    if (raw_fc <= 0) {
+        printf("You provided an invalid number of iterations. It has to be bigger than 0.\nThis App quit!\n");
+        return 1;   
+    }
+
+    if (raw_fc == 9223372036854775807) {
+        printf("WARNING: We have recived the biggest possible input number.\nIf this was intended you may ignore this warning.\nIf your number isn't 9223372036854775807, I will have to inform you: That is the maximum number we can do.\n");
+    }
+ 
+    final_step = (unsigned long)raw_fc;
+    used_digits = (size_t)floor(log10((double)final_step)) + 1;
+    
+    //Handle the input image
     raw_data_size = load_wbmp(original_frame, &width, &height, &current_structure);
     if (!raw_data_size > 0) {
         printf("The image at the provided path is invalid.\nPlease verify your path.\nThis App quit!\n");
         return 2;
     }
-    
-    printf("%s", current_structure);    
-    //testing
-    char *opath = "output.wbmp";
-    save_wbmp(opath, current_structure, width, height, raw_data_size);
-
+   
+    //Actual logic
+    printf("Loading complete, running loop:\n");
+    while (current_step < final_step){
+        printf("Rendering frame: %0*lu/", used_digits, current_step + 1);
+        printf("%lu\n", final_step);
+        //THINGS
+        //Tim da muesch du koche
+        current_step += 1;
+    }
+    printf("Rendered all frames, cleaning up!\n");
+ 
     //Cleanup before exit.
     free(current_structure);
     free(last_structure);
